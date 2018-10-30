cf4b367bf2202963cde6b28a49182227395c401a
Rory Lindsay <rorywork95@outlook.com>
2018-10-30 17:13:33 +0000

    added styling to home page, added js file and effects

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    
+
+    $(".bg-image").mouseenter(function(){
+       $(".landing").css("visibility", "visible");
+    });
+    
+    $(".bg-image").mouseleave(function(){
+       $(".landing").css("visibility", "hidden");
+        
+    });
+   
+    
+    
+});
+    
+        
