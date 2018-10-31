94de2930acc42bf8faf5367567ff12a8147e4e5e
Rory Lindsay <rorywork95@outlook.com>
2018-10-31 13:25:45 +0000

    added modal to index.html and styled

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     });
     
     $(".bg-image").mouseleave(function(){
-       $(".landing").css("visibility", "visible);
+       $(".landing").css("visibility", "visible");
         
     });
    
